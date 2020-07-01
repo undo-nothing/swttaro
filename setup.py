73055bb84379bc1bc0e77e7e01f62217ce212c4f
damien.yang <damien.yang@zkteco.com>
2020-07-01 15:58:45 +0800

    chore: add package function

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="swttaro",
+    version="0.0.1",
+    description="python common code personal repository",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'googletrans',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
